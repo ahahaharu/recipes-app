e87148ab943d4797928e29b47ed498f7874a25e3
Andrey <112519966+ahahaharu@users.noreply.github.com>
2026-02-11 00:36:29 +0300

    Fixed ts errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { routeTree } from './routeTree.gen';
-import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { GlobalStyles } from './globalStyles';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './theme';
-import { AuthProvider, useAuth } from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 import { ApolloProvider } from '@apollo/client/react';
 import { appoloClient } from './api/appolo';
+import App from './App';
 
 const queryClient = new QueryClient();
-
-const router = createRouter({
-  routeTree,
-  context: {
-    auth: undefined!,
-  },
-  defaultPreload: 'intent',
-});
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router;
-  }
-}
-
-function App() {
-  const auth = useAuth();
-
-  return <RouterProvider router={router} context={{ auth }} />;
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
